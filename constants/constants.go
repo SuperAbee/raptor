a6e29dc1883e36bea6165882b46058ec6b63c62c
clareyao(姚新聪) <clareyao@tencent.com>
2022-01-23 21:17:22 +0800

    update constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,5 +1,8 @@
 package constants
 
 const (
-	NACOS_GROUP = "raptor"
+	NACOS_GROUP               = "raptor"
+	K8S_NAMESPACE             = "default"
+	K8S_CONFIGMAP_CONTENT_KEY = "content"
+	K8S_APP_LABEL             = "raptor"
 )
