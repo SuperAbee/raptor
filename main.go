8179d08abfead527db3fc1cc5ae5a8912305b1f5
SuperAbee <yaoxincong@outlook.com>
2022-03-22 12:53:08 +0800

    add flags

--- main.go
+++ main.go
@@ -1,46 +1,27 @@
 package main
 
 import (
+	"flag"
+	"raptor/configcenter"
 	"raptor/router"
+	"raptor/servicecenter"
 
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	sc = flag.String("servicecenter", servicecenter.Memory, "Type of Service Center, Memory for Default.")
+	cc = flag.String("configcenter", configcenter.Memory, "Type of Config Center, Memory for Default.")
+)
+
 func main() {
+	flag.Parse()
+	servicecenter.Type = *sc
+	configcenter.Type = *cc
+
 	r := gin.New()
 	router.Route(r)
 
 	r.Run(":1234")
-
-	// sc.Register(servicecenter.RegisterParam{
-	// 	Ip:          "127.0.0.1",
-	// 	Port:        8888,
-	// 	ServiceName: "demo",
-	// })
-
-	// service, _ := sc.GetService("demo")
-	// log.Printf("%+v\n", service)
-
-	// cc, _ := configcenter.New(configcenter.Nacos)
-
-	// cc.Save(configcenter.Config{
-	// 	ID:      "config-1",
-	// 	Content: "hello time " + time.Now().String(),
-	// })
-	// time.Sleep(time.Second)
-
-	// config, _ := cc.Get("config-1")
-	// log.Printf("%+v\n", config)
-	// time.Sleep(time.Second)
-
-	// cc.OnChange("config-1", func(config configcenter.Config) {
-	// 	log.Printf("config update: %+v\n", config)
-	// })
-
-	// cc.Save(configcenter.Config{
-	// 	ID:      "config-1",
-	// 	Content: "hello time " + time.Now().String(),
-	// })
-	// time.Sleep(time.Second * 1000)
 
 }
